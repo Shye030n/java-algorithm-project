c9d89d91a7cbcd24ca260dda861b2a7d939c6fa1
Shye030n <shyeo_on@naver.com>
2026-02-01 15:23:38 +0900

    뭔가 잘못된 걸 느꼈지만, 그냥 해봤는데 방향을 바꾸는게 맞는듯. 정규식으로

--- src/kakao/p2/p02_3.java
+++ src/kakao/p2/p02_3.java
@@ -6,32 +6,45 @@ public class p02_3 {/* kakao HINT
  * - Semantic Analysis
  * - Regular Expression
  * */
+    //결론, split을 이용해서 문자열을 세 묶음으로 자르려고 시도했으나, 하나씩 꺼내서 배열로 저장하고 출력하는 방법으로 해야함.
+
+
     public static void main(String[] args) {
+
         //입력
         String one = "1S2D*3T"; //37
         //String two = "1D2S#10S"; //9
         //String three = "1D2S0T"; //3
 
+        String[] tokens; //분리된 문자열 저장소
+        String token; //분리된 문자
+
+        int a; //득점
+        int b; //보너스
+        int c; //옵션
+
 
         int score = 0; //점수 계산 저장소
 
-        String[] tokens; //분리된 문자열 저장소
-
         //처리
-        tokens = one.split("^[1-9]"); // 1~9로 시작하는 부분을 구분자로 지정하여 tokens에 저장
+        tokens = one.split(""); // 하나씩 잘라서 배열에 넣음 //근데 이것보다 정규식으로 바로 자르는게 좋을듯
         for(int i = 0; i < tokens.length; i++) {
-            System.out.println(tokens[i]);
+            System.out.println(tokens[i]); //하나씩 잘라내서 배열에 저장 완료
+            token = tokens[i];
+            if(token.equals("[/d]")) {
+                //score = ;
+            }else if(token.equals("[/w]")) {
+
+            }else if(token.equals("[\\#|\\*]")) {
+
+            } else return;
+
+
+
+
         }
-        //System.out.println(tokens.length);
-        //System.out.println(tokens[0]);
-        //System.out.println(tokens[1]);
-        //System.out.println(tokens[2]);
-        //    System.out.println(tokens[i]);
-
-
-
-
-
         //출력
+        //score = a*b*c
+
     }
 }
