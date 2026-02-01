41ab0f2c994e174fe4f85f90d76c4776c40c6313
Shye030n <shyeo_on@naver.com>
2026-02-01 15:02:52 +0900

    split()으로 자르니, 계산할 점수 자체가 문자열 하나라서 3개로 분리가 안됨. 처음부터 정규식을 해보자

--- src/kakao/p2/p02_3.java
+++ src/kakao/p2/p02_3.java
@@ -1,10 +1,36 @@
 package kakao.p2;
 
-public class p02_3 {
+public class p02_3 {/* kakao HINT
+ * - String Manipulation
+ * - Tokenizing
+ * - Semantic Analysis
+ * - Regular Expression
+ * */
     public static void main(String[] args) {
         //입력
+        String one = "1S2D*3T"; //37
+        //String two = "1D2S#10S"; //9
+        //String three = "1D2S0T"; //3
+
+
+        int score = 0; //점수 계산 저장소
+
+        String[] tokens; //분리된 문자열 저장소
 
         //처리
+        tokens = one.split("^[1-9]"); // 1~9로 시작하는 부분을 구분자로 지정하여 tokens에 저장
+        for(int i = 0; i < tokens.length; i++) {
+            System.out.println(tokens[i]);
+        }
+        //System.out.println(tokens.length);
+        //System.out.println(tokens[0]);
+        //System.out.println(tokens[1]);
+        //System.out.println(tokens[2]);
+        //    System.out.println(tokens[i]);
+
+
+
+
 
         //출력
     }
