c702af583517ace52a9c45f7640b3f92a08f8106
Shye030n <shyeo_on@naver.com>
2026-02-02 21:29:10 +0900

    자바에서 정규식을 지원하는 Pattern과 Matcher 클래스 연습 예제

---
+++ src/practice/Pattern_Matcher_Classes.java
@@ -0,0 +1,20 @@
+package practice;
+
+import java.util.regex.Pattern;
+
+public class Pattern_Matcher_Classes {
+    public static void main(String[] args) {
+        String patternString = "^[0-9]*$"; //숫자만 입력받는 패턴
+        Pattern pattern = Pattern.compile(patternString); //Pattern 클래스 객체로 정규식 컴파일
+
+        //샘플 문자열
+        String txt1 = "123456";
+        String txt2 = "1이히히6a";
+
+        boolean result1 = Pattern.matches(patternString, txt1);
+        boolean result2 = Pattern.matches(patternString, txt2);
+        System.out.println("result1 는 " + result1);
+        System.out.println("result2 는 " + result2);
+
+    }
+}
