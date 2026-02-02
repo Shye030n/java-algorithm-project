c631cf784ab9b6f5b38d8c801d37ea1d253df4a6
Shye030n <shyeo_on@naver.com>
2026-02-02 21:36:40 +0900

    패턴,매쳐클래스로 정규식 연습

--- src/practice/StringClass_Regex2.java
+++ src/practice/StringClass_Regex2.java
@@ -2,10 +2,10 @@ package practice;
 
 import java.util.regex.Pattern;
 
-public class Pattern_Matcher_Classes {
+public class StringClass_Regex2 {
     public static void main(String[] args) {
         String patternString = "^[0-9]*$"; //숫자만 입력받는 패턴
-        Pattern pattern = Pattern.compile(patternString); //Pattern 클래스 객체로 정규식 컴파일
+        //Pattern pattern = Pattern.compile(patternString); //Pattern 클래스 객체로 정규식 컴파일
 
         //샘플 문자열
         String txt1 = "123456";
