46e0b10218534faa8ab7cfeccbe125c405b02e40
shyeo_on <shyeo_on@naver.com>
2026-03-24 18:47:07 +0900

    구분자 추가, 대소문자구분X, 캐시사이즈 0인경우 추가. 캐시의 중간 인덱스에서 cacheHit인 경우 처리 로직 추가 필요

--- src/kakao/p3/p03_1.java
+++ src/kakao/p3/p03_1.java
@@ -7,10 +7,7 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /*
-* TODO
-*  1. 입출력 예제 1번 정상 값 출력
-*   입출력 예제 2번 값 오류
-*  2. 캐시 크기 0인 경우
+* TODO : 캐시의 중간에서 cacheHit인 경우, 처리 로직 추가 필요
 */
 
 public class p03_1 {
@@ -26,6 +23,11 @@ public class p03_1 {
     }
 
     public void access(String city) {
+        if(cacheSize == 0) { //cacheSize == 0
+            total += 5;
+            return; //더 이상 로직 진행하지 않음
+        }
+
         if (cities.contains(city)) { //cacheHit
             cities.remove(city);
             cities.add(0, city);
@@ -38,6 +40,7 @@ public class p03_1 {
             total += 5;
         }
     }
+    //캐시의 중간에서 cacheHit이 발생했을 때, 한 칸씩 앞으로 당길 메서드
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,11 +48,14 @@ public class p03_1 {
         //여기서, 캐시 사이즈 입력
         p03_1 lru = new p03_1(cacheSize);
         //여기서, 도시 이름 배열 입력
-        StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+        StringTokenizer st = new StringTokenizer(br.readLine(), ",[]\u201C\u201D"); //StringTokenizer는 두 번째 파라미터로 쌍따옴표 안에 있는 모든 문자들을 구분자로 인식 “”
         while (st.hasMoreTokens()) { //전체 토큰 반복
-            String city = st.nextToken(); //다음 토큰을 문자열 변수 city에 저장
+            String city = st.nextToken().toUpperCase(); //다음 토큰을 모두 대문자로 변환하여, 문자열 변수 city에 저장
             lru.access(city);
+            System.out.print(city);
         }
-        System.out.println(lru.total);
+        System.out.println();
+        System.out.println("실행 시간 : " + lru.total);
+        System.out.println("마지막 캐시 상태: " + lru.cities); //TODO 가운데가 비어있음. 이 말은 중간에서 데이터를 빼낸 인덱스가 공백으로 유지되는 상태인 거 같다
     }
 }
